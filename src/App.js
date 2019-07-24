260d624312e27edcce956d4f002a618208c24552
Georgy <mishin-georgijj@rambler.ru>
2019-07-25 01:03:55 +0300

    Create days example

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
 import React from "react";
 import { Case, Switch } from "./components";
 
-function App() {
+const App = () => {
+  const [currentTime, setCurrentTime] = React.useState(new Date().getDay());
+
+  React.useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentTime(new Date().getDay());
+    }, 1000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
   return (
-    <Switch value="121">
-      <Case value="12">
-        <div />
-      </Case>
-      <Case default>131</Case>
+    <Switch value={currentTime}>
+      <Case value={0}>Sunday</Case>
+      <Case value={1}>Monday</Case>
+      <Case value={2}>Tuesday</Case>
+      <Case value={3}>Wednesday</Case>
+      <Case value={4}>Thursday</Case>
+      <Case value={5}>Friday</Case>
+      <Case value={6}>Saturday</Case>
     </Switch>
   );
-}
+};
 
 export default App;
